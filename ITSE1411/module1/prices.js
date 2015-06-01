c306bd0646c12353f846492ce676963a783e9475
jeffnic <ffej.slohcin@gmail.com>
2015-05-31 20:59:36 -0500

    module1 project1 sunday night

---
+++ ITSE1411/module1/prices.js
@@ -0,0 +1,37 @@
+var bv6cost = 12.00
+var bv8cost = 15.00
+var ch4cost = 8.00
+var ch6cost = 11.00
+document.write("<div>")
+document.write("<ul>");
+document.write("<b>Bud Vases</b>");
+document.write("<ul><li>");
+document.write("6 \" Bud Vase - $" + bv6cost);
+document.write("</li>");
+document.write("<li>");
+document.write("8 \" Bud Vase - $" + bv8cost);
+document.write("</li>");
+document.write("</ul>");
+document.write("</div>")
+
+
+document.write("<div>")
+document.write("<b>Candle Holders</b>");
+//document.write("<ul>");
+document.write("<ul><li>");
+document.write("4 \" Candle Holder - $" + bv8cost);
+document.write("</li>");
+document.write("<li>");
+document.write("6 \" Candle Holder - $" + bv6cost);
+document.write("</li></ul>");
+document.write("</div>")
+
+
+document.write("<li></li>");
+
+
+document.write("");
+document.write("");
+document.write("");
+document.write("");
+document.write("</ul>");
