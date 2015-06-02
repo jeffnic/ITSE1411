220b5b57b4376c7cbc37586feb54c27c7b09f5a2
jeffnic <ffej.slohcin@gmail.com>
2015-06-01 21:03:40 -0500

    Monday night 6/1 updates

--- ITSE1411/module1/prices.js
+++ ITSE1411/module1/prices.js
@@ -2,37 +2,37 @@ var bv6cost = 12.00
 var bv8cost = 15.00
 var ch4cost = 8.00
 var ch6cost = 11.00
-var oc8cost = 20
-var oc10cost = 25
-var taxCost = 8
-var shipCost = 12
+var oc8cost = 20.00
+var oc10cost = 25.00
+var taxCost = 8.00
+var shipCost = 12.00
 
 document.write("<div>")
 document.write("<ul>");
 document.write("<b>Bud Vases</b>");
 document.write("<ul><li>");
-document.write("6 \" Bud Vase - $" + bv6cost.toFixed(2));
+document.write("6\" Bud Vase - $" + bv6cost.toFixed(2));
 document.write("</li>");
 document.write("<li>");
-document.write("8 \" Bud Vase - $" + bv8cost.toFixed(2));
+document.write("8\" Bud Vase - $" + bv8cost.toFixed(2));
 document.write("</li></ul>");
 document.write("<br>");
 
 document.write("<b>Candle Holders</b>");
 document.write("<ul><li>");
-document.write("4 \" Candle Holder - $" + bv8cost.toFixed(2));
+document.write("4\" Candle Holder - $" + bv8cost.toFixed(2));
 document.write("</li>");
 document.write("<li>");
-document.write("6 \" Candle Holder - $" + bv6cost.toFixed(2));
+document.write("6\" Candle Holder - $" + bv6cost.toFixed(2));
 document.write("</li></ul>");
 document.write("<br>");
 
 document.write("<b>Oil Candles</b>");
 document.write("<ul><li>");
-document.write(" 8 \" Oil Candle - $" + oc8cost.toFixed(2));
+document.write(" 8\" Oil Candle - $" + oc8cost.toFixed(2));
 document.write("</li>");
 document.write("<li>");
-document.write("10 \" Oil Candle - $" + oc10cost.toFixed(2));
+document.write("10\" Oil Candle - $" + oc10cost.toFixed(2));
 document.write("</li></ul>");
 document.write("</ul>");
 document.write("</div>")
